bbc4c36a0963ca938f1016de01a77c7fa29eabd2
king-peach <wtiroo@163.com>
2021-12-15 19:02:18 +0800

    deploy

---
+++ assets/js/22.0b836d61.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{480:function(t,e,n){"use strict";n.r(e);var s=n(25),u=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("custom page")])}),[],!1,null,null,null);e.default=u.exports}}]);
